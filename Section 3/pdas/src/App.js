4c08277e71f0b1da3d6b816ec00986c415ef9156
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2023-05-30 22:41:01 +0530

    Reuasable component | configurable component | props | tailwindcss config

--- Section 3/pdas/src/App.js
+++ Section 3/pdas/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import data from './constant/data';
 import ProfileCard from './components/ProfileCard';
+import Header from './components/Header';
 
 function App() {
+  let title = 'Personal Digital Assitants';
   return (
     <div className="App">
-      <h1>Personal Digital Assitants</h1>
+      <Header className='text-3xl font-bold underline' title={title} />
       <div className='digitalAssistant'>
         {data.map((item) => {
           return (<ProfileCard {...item} />)
